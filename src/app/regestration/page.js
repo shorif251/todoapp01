c6367ee7b676d225e927736f6b6a1a0301cc905b
shorif251 <syed.shorifbd@gmail.com>
2023-10-01 13:00:46 +0600

    added-l

--- src/app/regestration/page.js
+++ src/app/regestration/page.js
@@ -4,7 +4,7 @@ import UserRegestration from "../components/UserRegestration";
 export default function page() {
   return (
     <div>
-        <UserRegestration formTitle="Regestration on our system" url="http://127.0.0.1:3000/api/usersApi"/>
+        <UserRegestration formTitle="Regestration on our system" url="http://localhost:3000/api/usersApi"/>
     </div>
   )
 }
