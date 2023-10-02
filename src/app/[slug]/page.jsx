13339a00755ea6e23df41680e0b0adf5024bebd4
shorif251 <syed.shorifbd@gmail.com>
2023-10-03 02:03:03 +0600

    metaData feture added

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,27 +1,50 @@
 const getData = async(id) =>{
-  const res = await fetch(`http://localhost:3000/api/todosApi/${id}`, {cache:"no-cache"});
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {cache:"no-cache"});
   const resData = res.json();
   return resData;
 }
+
+export const generateMetadata = async({params}) =>{
+  const metaTitle = await getData(params.slug)
+  return{
+    title: metaTitle.title,
+  }
+}
+
+
+ 
 export default async function page({params}) {
   const {slug} = params
   const dynamicData = await getData(slug);
+
+ 
+
   const showDynamicData = ()=>{
     
-     if(dynamicData.success){
-      const {name, title, desc} = dynamicData.result;
-      return(
-        <div>
-          <h1>{name}</h1>
-          <h3>{title}</h3>
-          <p>{desc}</p>
-        </div>
-      )
-    }else{
-      return(
-        <h2>{dynamicData.result}</h2>
-      )
-    }
+    //  if(dynamicData.success){
+    //   const {name, title, desc} = dynamicData.result;
+    //   return(
+    //     <div>
+    //       <h1>{name}</h1>
+    //       <h3>{title}</h3>
+    //       <p>{desc}</p>
+    //     </div>
+    //   )
+    // }else{
+    //   return(
+    //     <h2>{dynamicData.result}</h2>
+    //   )
+    // }
+
+  
+    return (
+      <div>
+        {/* <h1>{name}</h1> */}
+          <h3>{dynamicData.title}</h3>
+          <p>{dynamicData.body}</p>
+      </div>
+    )
+
   }
 
   return (
